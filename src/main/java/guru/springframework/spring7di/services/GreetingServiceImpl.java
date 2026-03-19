e701637e1d327d87641b392d7be33b22a78eef2b
Matthieu Basset <matthieu.basset@proton.me>
2026-03-19 11:40:02 +0100

    Refactor tests/classes to use Spring DI

--- src/main/java/guru/springframework/spring7di/services/GreetingServiceImpl.java
+++ src/main/java/guru/springframework/spring7di/services/GreetingServiceImpl.java
@@ -1,6 +1,9 @@
 package guru.springframework.spring7di.services;
 
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class GreetingServiceImpl implements GreetingService {
     @Override
     public String sayGreeting() {
