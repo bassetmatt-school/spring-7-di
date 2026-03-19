d51657219a200d0408a9ccc38c41d00fc1273485
Matthieu Basset <matthieu.basset@proton.me>
2026-03-19 10:18:33 +0100

    Updating base files

--- src/test/java/guru/springframework/spring7di/Spring7DiApplicationTests.java
+++ src/test/java/guru/springframework/spring7di/Spring7DiApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Spring6DiApplicationTests {
+class Spring7DiApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
